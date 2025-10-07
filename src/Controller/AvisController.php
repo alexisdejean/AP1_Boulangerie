e92f44dd5e3e8f6792adf59880819c171ead1ac5
alexisdejean <alexis.dejean@campuscarloacutis.fr>
2025-10-07 20:46:45 +0200

    <feature>(mise en place de l'envoi d'avis)

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
+use App\Entity\Avis;
 use App\Repository\AvisRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,9 +13,26 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AvisController extends AbstractController
 {
     #[Route('/avis', name: 'app_avis')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em, AvisRepository $avisRepo): Response
     {
+        if($request->isMethod('POST')){
+            $note = $request->request->get('note');
+            $commentaire = $request->request->get('commentaire');
 
+            $avis = new Avis();
+            $avis->setNoteAvis((int) $note);
+            $avis->setCommentaireAvis($request->request->get('commentaire', ''));
+
+            $avis->setDateAvis(new \DateTime());
+            $avis->setIdUtilisateurAvis($this->getUser());
+
+
+            $em->persist($avis);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre avis a été soumis avec succès.');
+            return $this->redirectToRoute('app_avis');
+        }
         return $this->render('avis/index.html.twig', [
             'controller_name' => 'AvisController',
         ]);
